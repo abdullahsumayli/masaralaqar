05f44740f95b9569c3b4cd139515ab34661c84da
abdullah <sumayliabdullah@gmail.com>
2026-03-31 09:26:22 +0300

    refactor: rename saqr route to mq

---
+++ src/app/products/mq/MQLanding.tsx
@@ -0,0 +1,30 @@
+"use client";
+
+import { ScrollToHashOnMount } from "@/components/ScrollToHashOnMount";
+import { ProductPageLayout } from "@/components/product/ProductPageLayout";
+import { ProductHero } from "@/components/product/landing/ProductHero";
+import { ProblemSection } from "@/components/product/landing/ProblemSection";
+import { SolutionSection } from "@/components/product/landing/SolutionSection";
+import { FeaturesSection } from "@/components/product/landing/FeaturesSection";
+import { HowItWorks } from "@/components/product/landing/HowItWorks";
+import { BenefitsSection } from "@/components/product/landing/BenefitsSection";
+import { ProductDemo } from "@/components/product/landing/ProductDemo";
+import { PricingCTA } from "@/components/product/landing/PricingCTA";
+import { FinalCTA } from "@/components/product/landing/FinalCTA";
+
+export default function MQLanding() {
+  return (
+    <ProductPageLayout>
+      <ScrollToHashOnMount />
+      <ProductHero />
+      <ProblemSection />
+      <SolutionSection />
+      <FeaturesSection />
+      <HowItWorks />
+      <BenefitsSection />
+      <ProductDemo />
+      <PricingCTA />
+      <FinalCTA />
+    </ProductPageLayout>
+  );
+}
